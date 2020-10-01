47b35539a7959eb8963d6fbd62a68d93ec2cff87
fabricedemange <69899334+fabricedemange@users.noreply.github.com>
2020-10-01 23:08:12 +0200

    initialisation

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Blog-Forteroche
- 
+# jl_blogForteroche
+OC-P4/Blog de Jean Forteroche
---
+++ templates/edit_article.php
@@ -0,0 +1,8 @@
+<?php $this->title = "Modifier l'article"; ?>
+
+<div>
+
+<?php include('form_article.php');?>
+
+    <a class="text-white" href="../index.php">Retour à l'accueil</a>
+</div>
